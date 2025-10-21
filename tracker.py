efedea968535669e18f8b29efbc78fd1b0685d45
alexfurst1 <143307271+alexfurst1@users.noreply.github.com>
2025-10-20 20:22:28 -0400

    worked more on tracker commands, letting the tracker object handle most operations

--- tracker.py
+++ tracker.py
@@ -6,6 +6,7 @@ class Expense:
         self.amount = amount
         self.description = description
         self.date = date
+        self.id: int
         
         if not Expense.expenses:
             self.id = 1
@@ -13,12 +14,6 @@ class Expense:
             self.id = getattr(Expense.expenses[-1],"id")
         
         Expense.expenses.append(self)
-
-
-
-    def update_expense(self,new_amount: int, new_desc: str, new_date: str):
-        self.amount = new_amount
-        self.description = new_desc
 
     def delete(self):
         print(f"Manually deleting expense {self.id}")
@@ -28,9 +23,23 @@ class Expense:
         print(f"ID: {self.id} ")
 
 class ExpenseTracker:
+    expenses = []
 
     def __init__(self,expenses: list):
         self.expenses = Expense.expenses
 
-expense1 = Expense(30, "lunch","2025-09-13")
-print(expense1.id)
+    def add_expense(self):
+        expense = Expense()
+        expense.id = self.expenses[-1].id + 1
+        complete = False
+        while not complete:
+            try:
+                expense.amount = int(input("Enter expense amount: "))
+                expense.description = input("Enter a description for this expense: ")
+                expense.date = input("Enter a date for this expense in the format YYYY-MM-DD: ")
+            except ValueError, TypeError:
+                print("You may have entered something with the wrong format. Please try again")
+                continue
+            complete = True
+
+        
